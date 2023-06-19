4a0e9e5f6af49e364e34b1f69fda6a17ad27c7fc
Eugene <63323569+An-Eugene@users.noreply.github.com>
2023-06-19 17:30:25 +0300

    Update deploy.sh

--- rules/deploy.sh
+++ rules/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# parse rules_proxy and make PAC file
 mapfile -t lines < rules_proxy
 for ((i=0; i<${#lines[@]}; i++)); do
     line=${lines[i]}
@@ -16,3 +17,39 @@ sed -i '$ s/,$//' ../ss_conditions.pac
 echo "];" >> ../ss_conditions.pac
 
 cat ../templates/ss_conditions_template.pac >> ../ss_conditions.pac
+
+# parse both rules_proxy and rules_direct and make ACL file
+echo "[bypass_all]" > ../ss_conditions.acl
+echo "" >> ../ss_conditions.acl
+echo "[proxy_list]" >> ../ss_conditions.acl
+
+mapfile -t lines < rules_proxy
+for line in "${lines[@]}"; do
+    if [[ $line == *.*.*.* || $line != *.* ]]; then
+        transformed_line="$line"
+    elif [[ $line == *.*.* ]]; then
+        transformed_line="^$line$"
+    elif [[ $line == *.* ]]; then
+        transformed_line="(?:^|\.)$line$"
+    else
+        transformed_line="$line"
+    fi
+    echo "$transformed_line" >> ../ss_conditions.acl
+done
+
+echo "" >> ../ss_conditions.acl
+echo "[bypass_list]" >> ../ss_conditions.acl
+
+mapfile -t lines < rules_direct
+for line in "${lines[@]}"; do
+    if [[ $line == *.*.*.* || $line != *.* ]]; then
+        transformed_line="$line"
+    elif [[ $line == *.*.* ]]; then
+        transformed_line="^$line$"
+    elif [[ $line == *.* ]]; then
+        transformed_line="(?:^|\.)$line$"
+    else
+        transformed_line="$line"
+    fi
+    echo "$transformed_line" >> ../ss_conditions.acl
+done
