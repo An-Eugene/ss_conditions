fb6c284a39cd4b6cbbccba8bb1b2bd450b3a3675
Eugene <63323569+An-Eugene@users.noreply.github.com>
2023-06-19 17:08:07 +0300

    Update deploy.sh

--- rules/deploy.sh
+++ rules/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Read file 1.txt and modify lines with a single dot
 mapfile -t lines < rules_proxy
 for ((i=0; i<${#lines[@]}; i++)); do
     line=${lines[i]}
@@ -9,12 +8,11 @@ for ((i=0; i<${#lines[@]}; i++)); do
     fi
 done
 
-# Create the output file out1.txt
 echo "var __BLOCKEDSITES__ = [" > ../ss_conditions.pac
 for line in "${lines[@]}"; do
-    echo "\"$line\"," >> ../ss_conditions.pac
+    echo "  \"$line\"," >> ../ss_conditions.pac
 done
+sed -i '$ s/,$//' out1.txt
 echo "];" >> ../ss_conditions.pac
 
-# Append the content of file 2.txt to out1.txt
 cat ../templates/ss_conditions_template.pac >> ../ss_conditions.pac
