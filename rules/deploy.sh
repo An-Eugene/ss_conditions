97b748cfc0e2256997cd528a8b235732fedacace
Eugene <63323569+An-Eugene@users.noreply.github.com>
2023-06-19 17:03:02 +0300

    Update deploy.sh

--- rules/deploy.sh
+++ rules/deploy.sh
@@ -12,9 +12,9 @@ done
 # Create the output file out1.txt
 echo "var __BLOCKEDSITES__ = [" > ../ss_conditions.pac
 for line in "${lines[@]}"; do
-    echo "\"$line\"," >> out1.txt
+    echo "\"$line\"," >> ../ss_conditions.pac
 done
-echo "];" >> out1.txt
+echo "];" >> ../ss_conditions.pac
 
 # Append the content of file 2.txt to out1.txt
 cat ../templates/ss_conditions_template.pac >> ../ss_conditions.pac
